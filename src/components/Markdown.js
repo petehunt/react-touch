94a31089bed22758d3f7789269a3b80cfbf50f99
petehunt <floydophone@gmail.com>
2013-10-03 14:24:53 -0700

    commonjs-ification

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,16 +2,16 @@
 
 var React = require('React');
 
+var showdown = require('showdown');
+
+var converter = new showdown.converter();
+
 var Markdown = React.createClass({
-  componentWillMount: function() {
-    // TODO: find a version of Showdown on npm that browserifies correctly
-    this.converter = new Showdown.converter();
-  },
   render: function() {
     return (
       <div
         dangerouslySetInnerHTML={{
-          __html: this.converter.makeHtml(this.props.children)
+          __html: converter.makeHtml(this.props.children)
         }}
       />
     );
