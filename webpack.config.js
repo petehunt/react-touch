b9a11b587e18557dfc82251f3f33ae43bcdd0505
Tobias Koppers <tobias.koppers@googlemail.com>
2013-10-14 14:43:23 +0200

    get it working with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css/, loader: "style-loader!css-loader" },
-    ],
-    postLoaders: [
+      { test: /\.gif/, loader: "url-loader?limit=10000&minetype=image/gif" },
       { test: /\.js$/, loader: "jsx-loader" }
-    ]
+    ],
+    noParse: /parse-latest.js/
   }
 };
