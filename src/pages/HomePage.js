7a02c8c18eba86ca92ec33596ce35178c39902bb
petehunt <floydophone@gmail.com>
2013-11-17 17:04:27 -0800

    welcome message

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,10 +11,18 @@ var HomePage = React.createClass({
     return (
       <Layout className="HomePage">
         <h1><a href="http://reactjs.org/" target="_blank">React</a>-based touch demos</h1>
-        <p>Use the back button to get back to this menu.</p>
-        <p><a href="#glass">Frosted glass</a></p>
-        <p><a href="#viewer">Photo viewer</a></p>
-        <p><a href="#leftnav">Left nav</a></p>
+        <p>
+          The web isn't as performant as native, but on modern devices we can get pretty close.
+          Or at least a lot better than the Web's current reputation.
+          This set of demos demonstrates the power and performance of modern mobile devices (and
+          the React JS library). Physics and touch gesture recognition provided by
+          <a href="http://zynga.github.io/scroller" target="_blank">Zynga Scroller</a>.
+        </p>
+        <p>
+          The UX isn't that great (snapping is off, etc) but the performance is pretty good. Keep
+          in mind that this demo uses JavaScript to animate CSS3d transforms <strong>every frame</strong>
+          using React's data binding while targeting mobile devices.
+        </p>
       </Layout>
     );
   }
