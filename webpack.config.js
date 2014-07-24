4a65fa98723975b393670b26f95b5cb584483f4f
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-07-24 23:28:34 +0200

    Sorry, it's not *your* type
    :-)

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css/, loader: "style-loader!css-loader" },
-      { test: /\.gif/, loader: "url-loader?limit=10000&minetype=image/gif" },
-      { test: /\.jpg/, loader: "url-loader?limit=10000&minetype=image/jpg" },
-      { test: /\.png/, loader: "url-loader?limit=10000&minetype=image/png" },
+      { test: /\.gif/, loader: "url-loader?limit=10000&mimetype=image/gif" },
+      { test: /\.jpg/, loader: "url-loader?limit=10000&mimetype=image/jpg" },
+      { test: /\.png/, loader: "url-loader?limit=10000&mimetype=image/png" },
       { test: /\.js$/, loader: "jsx-loader" }
     ],
     noParse: /parse-latest.js/
